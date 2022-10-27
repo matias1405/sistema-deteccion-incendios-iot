a45ecfa8376b92a8b857c8ec725368e7ab76d140
matias alfaro <matiasalfaro1405@gmail.com>
2022-10-26 23:09:04 -0300

    agrega prueba de tension con el sensor de humo

--- test/test_sensorhumo.py
+++ test/test_sensorhumo.py
@@ -1,12 +1,35 @@
 import machine as m
 import time
+import math
 
-humo_input = m.Pin(34,m.Pin.IN)
-adc_humo = m.ADC(humo_input)
-adc_humo.atten(m.ADC.ATTN_6DB)
+
+PIN_SHUMO = 34 #gpio 34 y pin nro 5
+
+class SensorHumo:
+    """
+    Mide la concentracion de humo en ppm
+    """
+    def __init__(self, n_pin):
+        self.pin_s_humo = m.ADC(m.Pin(n_pin, m.Pin.IN))
+        self.pin_s_humo.atten(m.ADC.ATTN_11DB)
+        #self.calculos()
+        self.medir_humo()
+
+    def calculos(self):
+        voltaje_i = self.pin_s_humo.read()
+        voltaje_i = voltaje_i * 3.3 / 4096
+        self.R0 = 1000 * (5 - voltaje_i) / voltaje_i
+        self.R0 = self.R0 / 9.7
+        self.x1 = math.log10(200)
+        self.y1 = math.log10(3.43)
+    
+    def medir_humo(self):
+        self.ppm = self.pin_s_humo.read_uv()*1000
+        return self.ppm
+
+
+s_humo = SensorHumo(PIN_SHUMO)
 
 while True:
-    humo = adc_humo.read_uv()
-    humo =
-    print(humo)
-    time.sleep_ms(1000)
+    print("tension en mV detectado: ", s_humo.medir_humo())
+    time.sleep(5)
