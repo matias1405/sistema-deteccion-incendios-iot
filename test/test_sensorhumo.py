2f672fc6d45358238caf194696f03040745fea58
matias alfaro <matiasalfaro1405@gmail.com>
2022-10-27 00:09:27 -0300

    agrega prueba de ppm para el sensor de humo

--- test/test_sensorhumo.py
+++ test/test_sensorhumo.py
@@ -12,24 +12,33 @@ class SensorHumo:
     def __init__(self, n_pin):
         self.pin_s_humo = m.ADC(m.Pin(n_pin, m.Pin.IN))
         self.pin_s_humo.atten(m.ADC.ATTN_11DB)
-        #self.calculos()
+        self.calculos()
         self.medir_humo()
 
     def calculos(self):
-        voltaje_i = self.pin_s_humo.read()
-        voltaje_i = voltaje_i * 3.3 / 4096
+        voltaje_i = self.pin_s_humo.read_uv()/1000000
+        #voltaje_i = voltaje_i * 3.3 / 4096
         self.R0 = 1000 * (5 - voltaje_i) / voltaje_i
         self.R0 = self.R0 / 9.7
         self.x1 = math.log10(200)
         self.y1 = math.log10(3.43)
+        x2 = math.log10(10000)
+        y2 = math.log10(0.61)
+        self.curva = (y2 - self.y1)/(x2 - self.x1)
+
     
     def medir_humo(self):
-        self.ppm = self.pin_s_humo.read_uv()*1000
+        voltaje_i = self.pin_s_humo.read_uv()/1000000
+        RS = 1000 * (5 - voltaje_i) / voltaje_i
+        ratio = RS/self.R0
+        exponente = (math.log10(ratio)-self.y1)/self.curva
+        exponente = exponente + self.x1
+        self.ppm = 10**exponente
         return self.ppm
 
 
 s_humo = SensorHumo(PIN_SHUMO)
 
 while True:
-    print("tension en mV detectado: ", s_humo.medir_humo())
+    print("PPM de humo detectado: ", s_humo.medir_humo())
     time.sleep(5)
