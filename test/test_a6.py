2b72e04636fb633fa3636e4efe78b683bc934c71
matias alfaro <matiasalfaro1405@gmail.com>
2022-11-27 19:41:24 -0300

    mejoras al programa test_a6

--- test/test_a6.py
+++ test/test_a6.py
@@ -2,8 +2,8 @@ import machine as m
 import time
 import json
 
-PIN_TX_A6 = 1
-PIN_RX_A6 = 3
+PIN_TX_A6 = 1 #pin 35
+PIN_RX_A6 = 3 #pin 34
 
 u6 = m.UART(id=1, baudrate=9600, tx=PIN_TX_A6 , rx=PIN_RX_A6)
 time.sleep(1)
@@ -24,3 +24,11 @@ while True:
 
 send_at_command("AT+CREG?")
 time.sleep(5)
+send_at_command("AT+CGATT=1")
+time.sleep(1)
+send_at_command("AT+CSTT=\"wap.gprs.unifon.com.ar\",\"internet\",\"internet\"")
+time.sleep(3)
+send_at_command("AT+CIICR")
+time.sleep(3)
+send_at_command("AT+CIFSR")
+time.sleep(3)
