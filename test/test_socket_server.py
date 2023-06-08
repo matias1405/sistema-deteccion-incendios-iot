5c24f3e254ff619d2c4e3d048244afd17caeebee
matias alfaro <matiasalfaro1405@gmail.com>
2023-06-07 22:50:34 -0300

    modifica main de edge y test socket server

--- test/test_socket_server.py
+++ test/test_socket_server.py
@@ -1,4 +1,6 @@
 import socket
+import time
+
 def getIp():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -9,33 +11,34 @@ def getIp():
 
 ip = getIp()
 #print("primera ip conseguida", ip)
-#ip = '192.168.105.103'
+ip = "192.168.100.61"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((ip, 2020))
 print(ip)
 s.listen(10)
-(clientsocket, address) = s.accept()
-print(address)
 
+while True: 
+    COUNTER = 0
+    (clientsocket, address) = s.accept()
+    print(address)  
+    try:
+        while True:
+            data = clientsocket.recv(128)
+            data = data.decode()
+            if len(data) > 0:
+                print(data)
+                COUNTER += 1
+                print(COUNTER)
+                if COUNTER > 15:
+                    cadena = "STOP".encode()
+                    clientsocket.send(cadena)
+                else:
+                    cadena = "OK".encode()
+                    clientsocket.send(cadena)
+        
+    finally: 
+        clientsocket.close()
+    time.sleep(5)
 
-try:
-    while True:   
-        data = clientsocket.recv(256)
-        data = data.decode()
-        if len(data) > 0:
-            print(data)
-        if data == "INCENDIO":
-            cadena = "OK"
-            clientsocket.send(cadena.encode())
-            print("alerta incendio recibida")
-        elif data == "ADVERTENCIA":
-            cadena = "OK"
-            clientsocket.send(cadena.encode())
-            print("advertencia recibida")
-        elif data == "OK":
-            cadena = "OK"
-            clientsocket.send(cadena.encode())
-finally: 
-    clientsocket.close()
-    s.close()      
+s.close() 
