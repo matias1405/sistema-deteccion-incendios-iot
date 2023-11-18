c0f6595a6265ab5dab615165fc03e71529cfca79
matias alfaro <matiasalfaro1405@gmail.com>
2023-11-18 18:25:41 -0300

    num aleatorios para nodos fix

--- test/num_aleatorios_nodos.py
+++ test/num_aleatorios_nodos.py
@@ -2,8 +2,6 @@ import random
 import time
 from paho.mqtt import client as mqtt_client
 
-temperatura_original = []
-
 class Nodo:
     def __init__(self, name, t_mod_dist, t_mod_tiempo, h_mod_dist, h_mod_tiempo, t_lim, h_lim):
         self.name = name
@@ -18,6 +16,9 @@ class Nodo:
         self.t_topic = self.name + "/temperatura"
         self.h_topic = self.name + "/humo"
         self.pdf_topic = self.name + "/flama"
+        self.pdf = False
+        self.t = 0
+        self.h = 0
 
     def t_promedio(self):
         suma = 0
@@ -26,10 +27,10 @@ class Nodo:
         return suma/self.t_mod_tiempo
     
     def obtener_temp(self):
-        t = (self.t_promedio()-self.t_inicial)*(1-self.t_mod_dist)+self.t_inicial
+        self.t = (self.t_promedio()-self.t_inicial)*(1-self.t_mod_dist)+self.t_inicial
         if self.t_mod_dist > 0 :
             self.t_mod_dist = self.t_mod_dist - DISM_MOD_TIEMPO
-        return round(random.uniform(0.99*t, 1.01*t), 2)
+        return round(random.uniform(0.99*self.t, 1.01*self.t), 2)
     
     def h_promedio(self):
         suma = 0
@@ -38,40 +39,49 @@ class Nodo:
         return suma/self.h_mod_tiempo
     
     def obtener_humo(self):
-        h = (self.h_promedio()-self.h_inicial)*(1-self.h_mod_dist)+self.h_inicial
+        self.h = (self.h_promedio()-self.h_inicial)*(1-self.h_mod_dist)+self.h_inicial
         if self.h_mod_dist > 0 :
             self.h_mod_dist = self.h_mod_dist - DISM_MOD_TIEMPO
-        return round(random.uniform(0.99*h, 1.01*h), 2)
+        return round(random.uniform(0.99*self.h, 1.01*self.h), 2)
     
-    def publicar(self, client):
+    def publicar_t(self, client):
         temp = self.obtener_temp()
-        #print(f'la temperatura en {self.name} es de {temp} *C')
         client.publish(self.t_topic, temp)
-        humo = self.obtener_humo()
-        #print(f'la cantidad de humo en {self.name} es de {humo} ppm')
-        client.publish(self.h_topic, humo)
-        if (self.t_lim < temp and self.h_lim < humo):
-            client.publish(self.pdf_topic, "true")
+        if (self.t_lim < temp and self.h_lim < self.h):
+            if self.pdf == False:
+                client.publish(self.pdf_topic, "true")
         else:
-            client.publish(self.pdf_topic, "false")
+            if self.pdf == True:
+                client.publish(self.pdf_topic, "false")
+
+    def publicar_h(self, client):
+        humo = self.obtener_humo()
+        client.publish(self.h_topic, humo)
+        if (self.t_lim < self.t and self.h_lim < humo):
+            if self.pdf == False:
+                client.publish(self.pdf_topic, "true")
+        else:
+            if self.pdf == True:
+                client.publish(self.pdf_topic, "false")
 
 
 class Colas:
-    def __init__(self, valor_inicial, size_list):
-        self.valor_inicial = valor_inicial
-        self.lista = [valor_inicial for _ in range(size_list)]
+    def __init__(self, size_list):
+        self.size_list = size_list
+        self.inicio = True
         
     def add(self, temp):
-        self.lista.append(temp)
-        self.remove()
+        if self.inicio == True:
+            self.lista = [temp for _ in range(self.size_list)]
+            self.inicio = False
+        else:
+            self.lista.append(temp)
+            self.remove()
 
     def remove(self):
         self.lista.pop(0)
-
-
-
- #esto debe ir en la clase nodo donde la cantidad de elementos es el modificador de tiempo y el 30 es el modificador de distancia
     
+
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
@@ -88,24 +98,40 @@ def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         mensaje = msg.payload.decode()
         print(f"Received '{mensaje}' from '{msg.topic}' topic")
+        if msg.topic == "nodo-sensores/temperatura":
+            c_temperatura.add(float(mensaje))
+            nodo2.publicar_t(client)
+            time.sleep(1)
+            nodo3.publicar_t(client)
+            time.sleep(1)
+            nodo4.publicar_t(client)
+            time.sleep(1)
+            nodo5.publicar_t(client)
+            time.sleep(1)
+            nodo6.publicar_t(client)
+        else:
+            c_humo.add(float(mensaje))
+            nodo2.publicar_h(client)
+            time.sleep(1)
+            nodo3.publicar_h(client)
+            time.sleep(1)
+            nodo4.publicar_h(client)
+            time.sleep(1)
+            nodo5.publicar_h(client)
+            time.sleep(1)
+            nodo6.publicar_h(client)        
 
-    client.subscribe([("nodo-sensores/temperatura", 0), ("nodo-sensores/humo", 0), ("nodo-sensores/flama", 0)])
+    client.subscribe([("nodo-sensores/temperatura", 0), ("nodo-sensores/humo", 0)])
     client.on_message = on_message
 
-TEMP_LIMITE = 100
-TEMP_AUMENTO = 1
-HUMO_AUMENTO = 10000
 DISM_MOD_TIEMPO = 0.01
-inicio = True
-
 BROKER = 'localhost'
 PORT = 1883
 
-temp = float(input("temp_promedio_inicial: "))
-humo = float(input("ppm_humo_inicial: "))
-
-c_temperatura = Colas(temp, 60)
-c_humo = Colas(humo, 60)
+#temp = float(input("temp_promedio_inicial: "))
+#humo = float(input("ppm_humo_inicial: "))
+c_temperatura = Colas(60)
+c_humo = Colas(60)
 
 nodo2 = Nodo(name="nodo2", t_mod_dist=0.2, t_mod_tiempo=20, h_mod_dist=0.2, h_mod_tiempo=20, t_lim=40, h_lim=400000)
 nodo3 = Nodo(name="nodo3", t_mod_dist=0.3, t_mod_tiempo=30, h_mod_dist=0.3, h_mod_tiempo=30, t_lim=50, h_lim=500000)
@@ -113,29 +139,9 @@ nodo4 = Nodo(name="nodo4", t_mod_dist=0.4, t_mod_tiempo=40, h_mod_dist=0.4, h_mo
 nodo5 = Nodo(name="nodo5", t_mod_dist=0.5, t_mod_tiempo=50, h_mod_dist=0.5, h_mod_tiempo=50, t_lim=40, h_lim=600000)
 nodo6 = Nodo(name="nodo6", t_mod_dist=0.6, t_mod_tiempo=60, h_mod_dist=0.6, h_mod_tiempo=60, t_lim=50, h_lim=400000)
 
-if inicio == True:
-    inicio = False
-
-client = connect_mqtt()
-subscribe(client)
-client.loop_start()
 try:
-    while(True):
-        c_temperatura.add(temp)
-        #print("temperatura:", temp)
-        c_humo.add(humo)
-        #print("humo:", humo)
-        nodo2.publicar(client)
-        time.sleep(1)
-        nodo3.publicar(client)
-        time.sleep(1)
-        nodo4.publicar(client)
-        time.sleep(1)
-        nodo5.publicar(client)
-        time.sleep(1)
-        nodo6.publicar(client)
-        if(temp < 100):
-            temp = temp + TEMP_AUMENTO
-        if(humo < 600000):
-            humo = humo + HUMO_AUMENTO
-        time.sleep(1)
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_start()
+except Exception as e:
+    print(e)
